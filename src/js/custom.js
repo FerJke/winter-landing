cc06f48c7c58e3711309288981c691010ac5b302
Konstantin <bezvesilny.k@gmail.com>
2017-12-01 18:31:58 +0200

    add scroll

--- src/js/custom.js
+++ src/js/custom.js
@@ -44,6 +44,53 @@ function StartProject(link) {
 /* Add a scroll by click on the link in menu */
 function AddScroll(link){
 
+  function scrollTo(e){
+    e.preventDefault();
+
+    var id = e.target.getAttribute('href').substring(1);
+    var block = document.getElementById(id);
+    var blockTop = block.getBoundingClientRect().top;
+
+    var timerId = setInterval(function(){
+      if( blockTop > 0 ){
+        var sign = ">";
+        scollToElem(sign);
+      } else {
+        var sign = "<";
+        scollToElem(sign);
+      }
+    }, 10);
+
+    function scollToElem(sign){
+      if( sign == ">"  ) {
+
+        if( window.pageYOffset < blockTop ){
+          window.scrollBy(0, 10);
+        } else {
+          clearInterval(timerId);
+        }
+
+      } else {
+
+        if( block.getBoundingClientRect().top < 0 ){
+          window.scrollBy(0, -10);
+        } else {
+          clearInterval(timerId);
+        }
+
+      }
+
+    } // end function scollToElem;
+
+  }
+
+  this.links = document.querySelectorAll(link);
+
+  this.addListeners = function(){
+    for( var i = 0, num = this.links.length; i < num; i++ ){
+      this.links[i].addEventListener('click', scrollTo);
+    }
+  }
 }
 
 window.onload = function(){
@@ -52,7 +99,12 @@ window.onload = function(){
   var startProject = new StartProject('.js-scrollToStart');
   startProject.addListeners();
 
-  /*  */
-  var addScroll = new AddScroll('.navigation__link');
+  /* Scroll from header */
+  var addScrollHeader = new AddScroll('.navigation__link');
+  addScrollHeader.addListeners();
+
+  /* Scroll from footer */
+  var addScrollFooter = new AddScroll('.footer__nav a');
+  addScrollFooter.addListeners();
 
 }
